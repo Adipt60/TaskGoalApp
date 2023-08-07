7cebf41c782aeb2c014a43182e6cba2e60cbda3e
Aditya Patil <adipt1660@gmail.com>
2023-08-07 21:19:37 +0530

    Fixed Alignment

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,33 +20,57 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
         title: const Text("Task Goal"),
       ),
-      body: const Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
+      body: Column(
+        children: [
+          const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "50%",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Completed",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            Text(
-              "50%",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-              ),
-              textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            child: const Column(
+              children: [
+                Text(
+                  "Today",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Completed",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
